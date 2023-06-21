a40eaf2980a9b70e6b5cbe10bd612a6e0c57d937
nikita <paramonov20@bk.ru>
2023-06-21 19:48:51 +0300

    Научился передавать пареметры в модуль

--- app/src/main/java/ru/paramonov/startinjectdepenency/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/ru/paramonov/startinjectdepenency/example2/data/network/ExampleApiService.kt
@@ -1,12 +1,16 @@
 package ru.paramonov.startinjectdepenency.example2.data.network
 
+import android.content.Context
 import android.util.Log
+import ru.paramonov.startinjectdepenency.R
 import javax.inject.Inject
 
-class ExampleApiService @Inject constructor(){
+class ExampleApiService @Inject constructor(
+    private val context: Context
+){
 
     fun method() {
-        Log.d(LOG_TAG, "ExampleApiService")
+        Log.d(LOG_TAG, "ExampleApiService ${context.getString(R.string.app_name)}")
     }
 
     companion object {
--- app/src/main/java/ru/paramonov/startinjectdepenency/example2/di/ApplicationComponent.kt
+++ app/src/main/java/ru/paramonov/startinjectdepenency/example2/di/ApplicationComponent.kt
@@ -3,7 +3,7 @@ package ru.paramonov.startinjectdepenency.example2.di
 import dagger.Component
 import ru.paramonov.startinjectdepenency.example2.presentation.MainActivity
 
-@Component(modules = [DataModule::class, DomainModule::class])
+@Component(modules = [DataModule::class, DomainModule::class, ContextModule::class])
 interface ApplicationComponent {
 
     fun inject(activity: MainActivity)
