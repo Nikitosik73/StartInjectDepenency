f6035b57694c038a699f005c15cd339024a91f50
nikita <paramonov20@bk.ru>
2023-06-23 21:14:41 +0300

    Изучил обёртку Provider, которую предоставляет даггер в качестве возвращаемого значения

--- app/src/main/java/ru/paramonov/startinjectdepenency/example2/presentation/MainActivity2.kt
+++ app/src/main/java/ru/paramonov/startinjectdepenency/example2/presentation/MainActivity2.kt
@@ -1,16 +1,14 @@
 package ru.paramonov.startinjectdepenency.example2.presentation
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import ru.paramonov.startinjectdepenency.R
 import ru.paramonov.startinjectdepenency.example2.ExampleApp
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
@@ -33,11 +31,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewModel.method()
         viewModel2.method()
-
-        findViewById<TextView>(R.id.textView).setOnClickListener {
-            Intent(this, MainActivity2::class.java).apply {
-                startActivity(this)
-            }
-        }
     }
 }
